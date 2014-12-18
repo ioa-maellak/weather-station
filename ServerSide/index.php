9e130f019fd0c36a5f354887c2de31c67b1fc491
mbalamat <mbalamatsias@gmail.com>
2014-12-18 12:17:45 +0200

    License added to the files

--- ServerSide/index.php
+++ ServerSide/index.php
@@ -20,6 +20,23 @@
           <section class='col-xs-12 col-sm-6 col-md-6'>
 
 <?php
+
+/*
+Authors:
+Marios Balamatsias
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
 define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
 define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
--- ServerSide/insert.php
+++ ServerSide/insert.php
@@ -1,4 +1,21 @@
 <?php
+/*
+Authors:
+Marios Balamatsias
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
 define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT')); 
 define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
@@ -6,6 +23,7 @@ define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
 
 if (isset($_GET["rpiid"]) && isset($_GET["temp"]) && isset($_GET["hum"])){
+	
 	$servername = "localhost";
 	$username = DB_USER;
 	$password = DB_PASS;
