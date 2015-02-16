af28e771d97908e80f2de041102edbcfbcfda06a
mtsio <mtsio@mail.com>
2015-02-16 16:55:01 +0200

    order by desc

--- index.php
+++ index.php
@@ -5,17 +5,6 @@ Gerasimos Chamalis
 Loykianos-Nikolaos Xaxiris
 Anastasios Lisgaras
 Vasileios Karavasilis
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -->
 
 <!doctype html>
@@ -39,7 +28,7 @@ include_once 'config.php';
 $dbh = new PDO('mysql:dbname='.$dbname.';host='.$servername.';port='.$port, $username, $password);
 
 //Send the query.
-$sql = "SELECT * FROM metrics";
+$sql = "SELECT * FROM metrics ORDER BY when desc";
 $statement=$dbh->prepare($sql);
 $statement->execute();
 
