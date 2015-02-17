88d93448790d729cc525d9db2c22d11ccb048b8b
mtsio <mtsio@mail.com>
2015-02-18 00:27:21 +0200

    improve appearance

--- ServerSide/index.php
+++ ServerSide/index.php
@@ -5,19 +5,26 @@ Gerasimos Chamalis
 Loykianos-Nikolaos Xaxiris
 Anastasios Lisgaras
 Vasileios Karavasilis
+Tsiolkas Michalis
 -->
 
 <!doctype html>
 <html lang="en">
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <title>RPi Weather Station Database</title>
+    <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>Weather Station</title>
 
-  <link rel="stylesheet" type="text/css" href="mystyle.css">
-</head>
+	<link rel="stylesheet" type="text/css" href="mystyle.css">
+    </head>
 
-<body>
+    <body>
+	<h2>
+            a low cost weather station with AirPi -- check the code at
+            <a href="https://github.com/ioa-maellak/weather-station"> github
+            </a>
+        </h2>
+	<br>
 
 <?php
 include_once 'config.php';
@@ -26,24 +33,42 @@ include_once 'config.php';
 $dbh = new PDO('mysql:dbname='.$dbname.';host='.$servername.';port='.$port, $username, $password);
 
 //Send the query.
-$sql = "SELECT `id`, `when`, GROUP_CONCAT(CONCAT(`key`, ':', `value`) ORDER BY `key` SEPARATOR ',') as val FROM `metrics` GROUP BY `id`, `when` ORDER BY `when` desc;";
+$sql = "SELECT `id`, `when`, GROUP_CONCAT(`value` SEPARATOR '<td>') as val " . 
+       "FROM `metrics` GROUP BY `id`, `when` ORDER BY `when` desc;";
+
 $statement=$dbh->prepare($sql);
 $statement->execute();
 
-echo "<TABLE border='1'>";
+echo "<TABLE class=simpletable>" .
+     "<thead>" .
+     "<TR>" . 
+     "<th> Station </th>" .
+     "<th> Time </TH>" . 
+     "<TH> Carbon Monoxide </TH>" .
+     "<TH> Humidity </TH>" . 
+     "<TH> Light Level </TH>" . 
+     "<TH> Pressure </TH>" . 
+     "<TH colspan = 2> Temperature </TH>" .
+     "<TH> Noise </TH>" .
+     "</TR>" .
+     "</thead>" .
+     "<tbody>" ;
+
 //Get and display the results.
 while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-	$id = $row['id'];
-	$when = $row['when'];
-	$val = $row['val'];
+    $id = $row['id'];
+    $when = $row['when'];
+    $val = $row['val'];
 
-	echo "<TR>";
-	//echo "$id, $when, $key, $value <br>";
-	echo "<TD>$id</TD>";
-	echo "<TD>$when</TD>";
-	echo "<TD>$val</TD>";
-	echo "</TR>";
+
+    echo "<TR>";
+    //echo "$id, $when, $key, $value <br>";
+    echo "<TD>$id</TD>";
+    echo "<TD>$when</TD>";
+    echo "<TD>$val<TD>";
+    echo "</TR>";
 }
+echo "</tbody>";
 echo "</TABLE>";
 
 //Close connection.
