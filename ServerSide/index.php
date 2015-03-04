ca10a4a3449e485e803b3efb7f908b2ae19e23a8
mtsio <mtsio@mail.com>
2015-03-05 01:29:30 +0200

    add a button to load more data

--- ServerSide/index.php
+++ ServerSide/index.php
@@ -1,12 +1,12 @@
 <!--
-Authors:
-Marios Balamatsias
-Gerasimos Chamalis
-Loykianos-Nikolaos Xaxiris
-Anastasios Lisgaras
-Vasileios Karavasilis
-Tsiolkas Michalis
--->
+	  Authors:
+	  Marios Balamatsias
+	  Gerasimos Chamalis
+	  Loykianos-Nikolaos Xaxiris
+	  Anastasios Lisgaras
+	  Vasileios Karavasilis
+	  Tsiolkas Michalis
+	  -->
 
 <!doctype html>
 <html lang="en">
@@ -26,27 +26,43 @@ Tsiolkas Michalis
 	</h2>
 	<br>
 
-<?php
-include_once 'config.php';
+	<?php
+	include_once 'config.php';
 
-//Connect to database.
-$dbh = new PDO('mysql:dbname='.$dbname.';host='.$servername.';port='.$port, $username, $password);
-/* //Send the query.
-   $sql = ("SELECT `when`, GROUP_CONCAT(`value` SEPARATOR ' ') " .
-   "as val FROM `metrics`" .
-   "GROUP BY `id`, `when` ORDER BY `when` desc;"); */
 
-$sql = "SELECT * FROM metrics ORDER BY `when` DESC, `key` ASC;";
+	//Connect to database.
+	$dbh = new PDO('mysql:dbname='.$dbname.';host='.$servername.';port='.$port, $username, $password);
 
-$statement=$dbh->prepare($sql);
-$statement->execute();
+	/* //Send the query.
+	   $sql = ("SELECT `when`, GROUP_CONCAT(`value` SEPARATOR ' ') " .
+	   "as val FROM `metrics`" .
+	   "GROUP BY `id`, `when` ORDER BY `when` desc;"); */
 
-$sql = "SELECT unit FROM units;";
+	$sql = "SELECT count(*) as val_num FROM units";
+	$val_num = $dbh->prepare ($sql);
+	$val_num->execute ();
 
-$units = $dbh->prepare ($sql);
-$units->execute ();
+	$res = $val_num->fetch (PDO::FETCH_ASSOC);
+	$val_num = $res['val_num'];
 
-?>
+	if (! $_GET['lim']) {
+	    $lim = 105;
+	} else {
+	    $lim = $_GET['lim'];
+	}
+
+
+	$sql = "SELECT * FROM metrics ORDER BY `when` DESC, `key` ASC LIMIT $lim;";
+
+
+	$statement=$dbh->prepare($sql);
+	$statement->execute();
+
+	$sql = "SELECT unit FROM units;";
+	$units = $dbh->prepare ($sql);
+	$units->execute ();
+
+	?>
 	<table class=simpletable>
 	    <thead> 
 		<tr> 
@@ -62,34 +78,42 @@ $units->execute ();
 	    </thead>
 	    <tbody>
 
-<?php
-//Get and display the results.
+		<?php
+		//Get and display the results.
 
-$id = 'etepi';
+		$id = 'etepi';
 
-while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
-    $when = $row['when'];
-    $val = $row['value'];
+		while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+		    $when = $row['when'];
+		    $val = $row['value'];
 
-    $unit_row = $units->fetch (PDO::FETCH_ASSOC);
-    $unit = $unit_row['unit'];
-    
-    if ($when != $prev_when) {
-        echo "<tr>";
-        echo "<td>$id</td>";
-        echo "<td>$when</td>";
-        $prev_when = $when;
-    }
-    
-    echo "<td>$val $unit</td>";
-}
+		    $unit_row = $units->fetch (PDO::FETCH_ASSOC);
+		    $unit = $unit_row['unit'];
+		    
+		    if ($when != $prev_when) {
+			echo "<tr>";
+			echo "<td>$id</td>";
+			echo "<td>$when</td>";
+			$prev_when = $when;
+		    }
+		    
+		    echo "<td>$val $unit</td>";
+		}
 
-//Close connection.
-$dbh = null;
+		//Close connection.
+		$dbh = null;
 
-?>
 
-	    </tbody>
-	</table>
-    </body>
-</html>
+		print "</tbody>";
+		print "</table>";
+		print "<br>";
+		print "<form method=GET action='loadmore.pl'>";
+		print "<button type=submit name=lim value=$lim>";
+		print "<img src=graphics/more.png>";
+		print "</button>";
+		print "</form>";
+		
+		print "</body>";
+		print "</html>";
+
+		?>
