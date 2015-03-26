bb27a611e1200bf0cf5a52fb20684d595a4d6d42
mbalamat <mbalamatsias@gmail.com>
2015-03-26 16:33:04 +0200

    Fix getData script to output json

--- www/api/getData.php
+++ www/api/getData.php
@@ -1,38 +1,30 @@
 <?php
-	/*
-	Authors:
-	Marios Balamatsias
- 	*/
-	include_once 'config.php';
-	$db_name = 'met';
-	$db = 'metrics';	
-	$db=mysql_connect($host, $username, $password) or die('Could not connect');
-	mysql_select_db($db_name, $db) or die('');
-	$lim = 7;
-	$sql = "SELECT * FROM `metrics` LIMIT 0, 30 ";
-	$result = mysql_query($sql) or die('Could not query');
-	
-	if(mysql_num_rows($result)){
-    		echo '{"data":[';
-
-    		$first = true;
-    		$row=mysql_fetch_assoc($result);
-    	while($row=mysql_fetch_row($result)){
-        	//  cast results to specific data types
-
-        	if($first) {
-            		$first = false;
-        	} else {
-            		echo ',';
-        	}
-        	echo json_encode($row);
-    	}	
-    		echo ']}';
-	} else {
-    		echo '[]';
-	}
-	
-	
-	mysql_close($db);
-
->
+   /*
+   Authors:
+   Marios Balamatsias
+   */
+    include_once 'config.php
+    $db = mysql_connect($servername,$username,$password);
+    if (!$db) {
+        die('Could not connect to db: ' . mysql_error());
+    }
+ 
+    mysql_select_db($dbname,$db);
+    $sql = "SELECT * FROM `metrics` LIMIT 0, 30 ";    
+    $result = mysql_query($sql, $db);  
+    
+    $json_response = array();
+    
+    while ($row = mysql_fetch_array($result, MYSQL_ASSOC)) {
+        $row_array['id'] = $row['id'];
+        $row_array['when'] = $row['when'];
+        $row_array['key'] = $row['key'];
+        $row_array['value'] = $row['value'];
+        
+        array_push($json_response,$row_array);
+    }
+    echo json_encode($json_response);
+    
+    fclose($db);
+ 
+?>
